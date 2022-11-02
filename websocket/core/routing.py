973c176631a0ff222e2d98c0dced1e7ba68aa4f0
SAHIL <70153394+sahil564@users.noreply.github.com>
2022-11-02 18:22:38 +0530

    Add files via upload

---
+++ websocket/core/routing.py
@@ -0,0 +1,8 @@
+import imp
+from django.urls import path
+from . import consumers
+
+websocket_urlpatterns =[
+    path("ws/sc/",consumers.MySyncConsumer.as_asgi()),
+    path("ws/ac/",consumers.MyAsyncConsumer.as_asgi()),
+]
