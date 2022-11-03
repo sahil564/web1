8988bd9562a2aa031d83416b550ca66378dd4f64
SAHIL <70153394+sahil564@users.noreply.github.com>
2022-11-03 12:11:26 +0530

    Update settings.py

--- websocket/websocket/settings.py
+++ websocket/websocket/settings.py
@@ -13,9 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_%s*&rqi$!xg-)$y4o=h#e4i(w=g@+govc5+)j)!5=mt%=&hij'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
